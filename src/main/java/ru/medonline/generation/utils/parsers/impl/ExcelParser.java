31025b6f52bf3fcfc7c8cffac43df0af6b13ab5f
Antonov A.S <java.dev.antonov.a.s@gmail.com>
2024-04-03 13:33:34 +0100

    feature: добавлены парсеры

---
+++ src/main/java/ru/medonline/generation/utils/parsers/impl/ExcelParser.java
@@ -0,0 +1,12 @@
+package ru.medonline.generation.utils.parsers.impl;
+
+import ru.medonline.generation.utils.parsers.Parser;
+
+import java.util.List;
+
+public class ExcelParser implements Parser {
+    @Override
+    public List<String> parse(String inputData) {
+        return null;
+    }
+}
